67ac76bd98f5dff844931ff160b5c6c0ce535baf
guptan <nguptaa183@gmail.com>
2018-10-30 19:35:03 +0530

    changes

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def verify_fb_token(token_sent):
     #if they match, allow the request, else return an error
     if token_sent == VERIFY_TOKEN:
         return request.args.get("hub.challenge")
-    # return 'Invalid verification token'
+    return 'Invalid verification token'
 
 #chooses a random message to send to the user
 def get_message():
