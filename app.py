df2bbdae991ce7b6b1687a76b2294fbc7e3efe79
guptan <nguptaa183@gmail.com>
2018-10-30 20:03:19 +0530

    changes

--- app.py
+++ app.py
@@ -9,18 +9,17 @@ ACCESS_TOKEN = 'EAAesOYFlRUwBAFZAAW3XGpWQgvliso6FYtL0YtBjZAhYVoqIACBBC0vOoPE0ZCC
 VERIFY_TOKEN = 'schedule_bot'
 bot = Bot(ACCESS_TOKEN)
 
-#We will receive messages that Facebook sends our bot at this endpoint
 
 @app.route("/", methods=['GET', 'POST'])
 def receive_message():
     if request.method == 'GET':
-        """Before allowing people to message your bot, Facebook has implemented a verify token
-        that confirms all requests that your bot receives came from Facebook."""
-        token_sent = request.args.get("hub.verify_token")
-        return verify_fb_token(token_sent)
-    #if the request was not get, it must be POST and we can just proceed with sending a message back to user
-    else:
-        # get whatever message a user sent the bot
+        if request.args.get("hub.verify_token") == VERIFY_TOKEN:
+            return request.args.get("hub.challenge")
+        else:
+            return 'Invalid verification token'
+
+    elif request.method == 'POST':
+
        output = request.get_json()
        for event in output['entry']:
           messaging = event['messaging']
@@ -37,24 +36,18 @@ def receive_message():
                     send_message(recipient_id, response_sent_nontext)
     return "Message Processed"
 
-def verify_fb_token(token_sent):
-    #take token sent by facebook and verify it matches the verify token you sent
-    #if they match, allow the request, else return an error
-    if token_sent == VERIFY_TOKEN:
-        return request.args.get("hub.challenge")
-    return 'Invalid verification token'
+# def verify_fb_token(token_sent):
+#     if token_sent == VERIFY_TOKEN:
+#         return request.args.get("hub.challenge")
+#     return 'Invalid verification token'
 
-#chooses a random message to send to the user
 def get_message():
     sample_responses = ["You are stunning!", "We're proud of you.",
                         "Keep on being you!", "We're grateful to know you :)"]
-    # return selected item to the user
+
     return random.choice(sample_responses)
 
-#uses PyMessenger to send response to user
-
 def send_message(recipient_id, response):
-    #sends user the text message provided via input response parameter
     bot.send_text_message(recipient_id, response)
     return "success"
 
