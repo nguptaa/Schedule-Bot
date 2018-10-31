984ddd9bee1d85902ef442552ee1af0456481436
guptan <nguptaa183@gmail.com>
2018-10-31 13:40:53 +0530

    changes

--- prac.py
+++ prac.py
@@ -3,5 +3,7 @@ from tabulate import tabulate
 import pandas
 df = pandas.read_csv('s1.csv')
 print(df)
+print(df.loc[0][1])
+# print(df)
 response_sent_text = tabulate(df, tablefmt="grid")
 print(response_sent_text)
