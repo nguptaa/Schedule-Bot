16f31411e21cabef240787fba4b2c56d51bc2a95
guptan <nguptaa183@gmail.com>
2018-10-31 08:18:04 +0530

    changes

--- app.py
+++ app.py
@@ -9,17 +9,15 @@ ACCESS_TOKEN = 'EAAesOYFlRUwBAFZAAW3XGpWQgvliso6FYtL0YtBjZAhYVoqIACBBC0vOoPE0ZCC
 VERIFY_TOKEN = 'schedule_bot'
 bot = Bot(ACCESS_TOKEN)
 
-
 @app.route("/", methods=['GET', 'POST'])
 def receive_message():
     if request.method == 'GET':
         if request.args.get("hub.verify_token") == VERIFY_TOKEN:
             return request.args.get("hub.challenge")
-        else:
-            return 'Invalid verification token'
+        return 'Invalid verification token'
+    return 'Hello World', 200
 
     elif request.method == 'POST':
-
        output = request.get_json()
        for event in output['entry']:
           messaging = event['messaging']
@@ -28,24 +26,22 @@ def receive_message():
                 #Facebook Messenger ID for user so we know where to send response back to
                 recipient_id = message['sender']['id']
                 if message['message'].get('text'):
-                    response_sent_text = get_message()
+                    response_sent_text = get_text()
                     send_message(recipient_id, response_sent_text)
                 #if user sends us a GIF, photo,video, or any other non-text item
                 if message['message'].get('attachments'):
-                    response_sent_nontext = get_message()
+                    response_sent_nontext = get_attachments()
                     send_message(recipient_id, response_sent_nontext)
     return "Message Processed"
 
-# def verify_fb_token(token_sent):
-#     if token_sent == VERIFY_TOKEN:
-#         return request.args.get("hub.challenge")
-#     return 'Invalid verification token'
-
-def get_message():
+def get_text():
     sample_responses = ["You are stunning!", "We're proud of you.",
                         "Keep on being you!", "We're grateful to know you :)"]
 
     return random.choice(sample_responses)
+
+def get_attachments():
+    return "I've no idea what to do with a image or attachments :("
 
 def send_message(recipient_id, response):
     bot.send_text_message(recipient_id, response)
