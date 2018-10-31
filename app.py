ae374534c891aeea6f9a8457725a884fe37e16c9
guptan <nguptaa183@gmail.com>
2018-10-31 08:23:20 +0530

    changes

--- app.py
+++ app.py
@@ -1,51 +1,48 @@
-#Python libraries that we need to import for our bot
-import os
-import random
 from flask import Flask, request
-from pymessenger.bot import Bot
+from pymessenger import Bot
 
-app = Flask(__name__)
-ACCESS_TOKEN = 'EAAesOYFlRUwBAFZAAW3XGpWQgvliso6FYtL0YtBjZAhYVoqIACBBC0vOoPE0ZCCA3cc6eIOmv541LexG06jaKV0EwAPb6pwe8dZAtjb0Na9zzTvgRQW9MZCQQ3zZA1OruowbC6rqMShW8R8G8lt2Ds9v3ZBWWUe7TDqd9QPcrx2WewqeAHeO0mQ'
-VERIFY_TOKEN = 'schedule_bot'
+app = Flask("Schedule Bot")
+
+ACCESS_TOKEN = "EAAesOYFlRUwBAFZAAW3XGpWQgvliso6FYtL0YtBjZAhYVoqIACBBC0vOoPE0ZCCA3cc6eIOmv541LexG06jaKV0EwAPb6pwe8dZAtjb0Na9zzTvgRQW9MZCQQ3zZA1OruowbC6rqMShW8R8G8lt2Ds9v3ZBWWUe7TDqd9QPcrx2WewqeAHeO0mQ"
 bot = Bot(ACCESS_TOKEN)
 
-@app.route("/", methods=['GET', 'POST'])
-def receive_message():
-    if request.method == 'GET':
-        if request.args.get("hub.verify_token") == VERIFY_TOKEN:
-            return request.args.get("hub.challenge")
-        return 'Invalid verification token'
-    return 'Hello World', 200
+VERIFY_TOKEN = "schedule_bot"
 
-    else:
-       output = request.get_json()
-       for event in output['entry']:
-          messaging = event['messaging']
-          for message in messaging:
-            if message.get('message'):
-                #Facebook Messenger ID for user so we know where to send response back to
-                recipient_id = message['sender']['id']
-                if message['message'].get('text'):
-                    response_sent_text = get_text()
-                    send_message(recipient_id, response_sent_text)
-                #if user sends us a GIF, photo,video, or any other non-text item
-                if message['message'].get('attachments'):
-                    response_sent_nontext = get_attachments()
-                    send_message(recipient_id, response_sent_nontext)
-    return "Message Processed"
 
-def get_text():
-    sample_responses = ["You are stunning!", "We're proud of you.",
-                        "Keep on being you!", "We're grateful to know you :)"]
+@app.route('/', methods=['GET'])
+def verify():
+	if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+		if not request.args.get("hub.verify_token") == VERIFY_TOKEN:
+			return "Verification token mismatch", 403
+		return request.args["hub.challenge"], 200
+	return "Hello world", 200
 
-    return random.choice(sample_responses)
 
-def get_attachments():
-    return "I've no idea what to do with a image or attachments :("
+@app.route('/', methods=['POST'])
+def webhook():
+	print(request.data)
+	data = request.get_json()
 
-def send_message(recipient_id, response):
-    bot.send_text_message(recipient_id, response)
-    return "success"
+	if data['object'] == "page":
+		entries = data['entry']
+
+		for entry in entries:
+			messaging = entry['messaging']
+
+			for messaging_event in messaging:
+
+				sender_id = messaging_event['sender']['id']
+				recipient_id = messaging_event['recipient']['id']
+
+				if messaging_event.get('message'):
+					# HANDLE NORMAL MESSAGES HERE
+					if messaging_event['message'].get('text'):
+						# HANDLE TEXT MESSAGES
+						query = messaging_event['message']['text']
+						# ECHO THE RECEIVED MESSAGE
+						bot.send_text_message(sender_id, query)
+	return "ok", 200
+
 
 if __name__ == "__main__":
-    app.run()
+	app.run(port=8000, use_reloader=True)
